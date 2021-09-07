e8b8a502d5023264717495ccb215c2b92f33deef
Ramesh Ramakrishna <ram.events1@gmail.com>
2021-09-07 22:28:03 +0530

    Statement updates

--- src/creditcard/Moneyreport.java
+++ src/creditcard/Moneyreport.java
@@ -2,6 +2,6 @@ package creditcard;
 
 public class Moneyreport {
     public static void main(String[] args) {
-        System.out.println("Bank statement about money");
+        System.out.println("Bank statement about money report for ramesh");
     }
 }
