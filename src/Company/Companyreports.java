705bf834adb5c5777b62e5f744bcf183b5c58ef5
Ramesh Ramakrishna <ram.events1@gmail.com>
2021-09-08 13:58:24 +0530

    Updates

--- src/Company/Companyreports.java
+++ src/Company/Companyreports.java
@@ -2,6 +2,6 @@ package Company;
 
 public class Companyreports {
     public static void main(String[] args) {
-        System.out.println("Welcome to Software company");
+        System.out.println("Welcome to Software company to the report");
     }
 }
